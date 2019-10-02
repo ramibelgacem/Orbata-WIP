625258f5a530a6353bae8604e2a03b044ba841d9
Rami Belgacem <ramibelgacem@gmail.com>
2019-10-02 16:34:19 +0100

    [ADD] command line application

---
+++ cli.py
@@ -0,0 +1,48 @@
+# -*- coding: utf8 -*-
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="selframe",
+    usage='%(prog)s [options] keyword',
+    description="Selframe command line application",
+    epilog="Code well!",
+    allow_abbrev=False,
+)
+
+parser.add_argument(
+    'keyword',
+    type=str,
+    choices=['build', 'start'],
+    help="Build a project or start the server",
+)
+
+build_group = parser.add_argument_group('build', 'Build a project')
+build_group.add_argument(
+    '--name',
+    type=str,
+    help="The name of the new project",
+)
+
+start_group = parser.add_argument_group('start', 'Start the server')
+start_group.add_argument(
+    '--host',
+    type=str,
+    help="The host of the web server",
+    action='store',
+    default='127.0.0.1',
+)
+start_group.add_argument(
+    '--port',
+    type=int,
+    help="The port of the web server",
+    action='store',
+    default='5000',
+)
+
+args = parser.parse_args()
+if args.keyword == "build":
+    print("You want to build a project")
+if args.keyword == "start":
+    print("You want to build a project")
+if args.port:
+    print("Port")
