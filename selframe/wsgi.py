b862568fc8ad9aa9a85dbd1c98822ed9d6a4e612
Rami Belgacem <ramibelgacem@gmail.com>
2019-10-15 19:29:01 +0100

    Trying to find a solution for app structure

--- selframe/wsgi.py
+++ selframe/wsgi.py
@@ -1,8 +1,9 @@
 # -*- coding: utf8 -*-
+from werkzeug.serving import run_simple
 from werkzeug.wrappers import Request, Response
 
-from .route import Router
 from .logging import logger
+from .route import Router
 
 
 class WebApp(object):
@@ -33,3 +34,25 @@ class WebApp(object):
 
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
+
+
+def run(self,
+        app,
+        host='127.0.0.1',
+        port=5000,
+        use_reloader=False,
+        use_debugger=True):
+    """ Start a server instance.
+        :param app: WSGI application.
+        :param host: Server address to bind to. (default: 127.0.0.1)
+        :param port: Server port to bind to. (default: 5000)
+        :param use_reloader: Start auto-reloading server? (default: False)
+        :param use_debugger: Auto-reloader interval in seconds (default: 1)
+     """
+    run_simple(
+        host,
+        port,
+        app,
+        use_reloader=use_reloader,
+        use_debugger=use_debugger,
+    )
